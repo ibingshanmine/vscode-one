e63016fca02e939457f495d6ba6ec41016932788
iiceberg <100066546+ibingshanmine@users.noreply.github.com>
2022-02-21 16:19:09 +0800

    Update code1.c
    有感而发

--- c/code1.c
+++ c/code1.c
@@ -2,7 +2,13 @@
 //c语言是从主函数的第一行代码开始执行的
 int main()
 {
-    printf("你好\n");
+    printf("你好\n"); 
+    printf("hello\n");
+    printf("今天阳光明媚\n");
+    printf("可惜有风掠过\n");
+    printf("虽不刺骨\n"); 
+    printf("但依旧寒身\n");
+    
   
     return 0;
 }
